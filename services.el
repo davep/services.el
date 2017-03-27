514e4095e8964c4d0f38c4f3ad6c692e86d12faa
Dave Pearson <davep@davep.org>
2017-03-27 16:35:47 +0100

    v1.7

--- services.el
+++ services.el
@@ -2,7 +2,7 @@
 ;; Copyright 2000-2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.6
+;; Version: 1.7
 ;; Keywords: convenience, net, services
 ;; URL: https://github.com/davep/services.el
 ;; Package-Requires: ((cl-lib "0.5"))
